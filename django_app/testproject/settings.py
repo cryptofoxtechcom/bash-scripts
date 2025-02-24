82c9982034eddefa8fe89b3f0886bd7959fc1824
Carlos DaSilva <cryptofoxtechcom@gmail.com>
2025-02-24 03:54:33 -0500

    added django app

--- django_app/testproject/settings.py
+++ django_app/testproject/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'testproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangodb',
+        'USER': 'djangouser',
+        'PASSWORD': 'djangopassword',
+        'HOST': 'postgres_db',  # Use the service name from docker-compose.yml
+        'PORT': '5432',
     }
 }
 
